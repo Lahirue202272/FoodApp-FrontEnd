e4c66f3311301ddad1edd81d6d711049ba7c5a88
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-04-24 17:17:41 +0530

    implement the Login page,Categories page,Honepage,Mneupage,Menudetails page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Footer from "./components/common/Footer";
 import Navbar from "./components/common/Navbar";
 import RegisterPage from "./components/auth/RegisterPage";
+import LoginPage from "./components/auth/LoginPage";
+import HomePage from "./components/home_menu/HomePage";
+import CategoriesPage from "./components/home_menu/CategoriesPage";
+import MenuPage from "./components/home_menu/MenuPage";
+import MenuDetailsPage from "./components/home_menu/MenuDetailsPage";
 
 
 function App() {
@@ -11,6 +16,11 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/register" element={<RegisterPage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/menu/:id" element={<MenuDetailsPage />} />
         </Routes>
       </div>
       <Footer />
