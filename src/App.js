cde2d159a4927901c3dce1e9b45df56d586c1cdc
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-07-11 21:51:28 +0530

    implement AdminCategoriesPage and AdminLayout components with routing

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ import MenuDetailsPage from "./components/home_menu/MenuDetailsPage";
 import ProfilePage from "./components/profile_cart/ProfilePage";
 import UpdateProfilePage from "./components/profile_cart/UpdateProfilePage";
 import OrderHistoryPage from "./components/profile_cart/OrderHistoryPage";
+import { AdminRoute, CustomerRoute } from "./services/Guard";
 import LeaveReviewPage from "./components/profile_cart/LeaveReviewPage";
 import CartPage from "./components/profile_cart/CartPage";
+import AdminLayout from "./components/admin/navbar/AdminLayout";
+import AdminCategoriesPage from "./components/admin/AdminCategoriesPage";
 
 
 function App() {
@@ -21,19 +24,29 @@ function App() {
       <Navbar />
       <div className="content">
         <Routes>
+          {/* AUTH PAGES */}
           <Route path="/register" element={<RegisterPage/>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/categories" element={<CategoriesPage />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/menu/:id" element={<MenuDetailsPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/update" element={<UpdateProfilePage />} />
-          <Route path="/my-order-history" element={<OrderHistoryPage />} />
-          <Route path="/leave-review" element={<LeaveReviewPage />} />
-          <Route path="/cart" element={<CartPage />} />
+
+          {/* CUSTOMER ROUTES */}
+          <Route path="/profile" element={<CustomerRoute component={<ProfilePage />} />} />
+          <Route path="/update" element={<CustomerRoute element={<UpdateProfilePage />} />} />
+          <Route path="/my-order-history" element={<CustomerRoute element={<OrderHistoryPage />} />} />
+          <Route path="/leave-review" element={<CustomerRoute element={<LeaveReviewPage />} />} />
+          <Route path="/cart" element={<CustomerRoute element={<CartPage />} />} />
+
+           {/* ADMIN ROUTES */}
+          <Route path="/admin" element={<AdminRoute element={<AdminLayout />} />}>
+             <Route path="categories" element={<AdminCategoriesPage />} />
+          </Route>
+
 
           <Route path="*" element={<Navigate to={"/home"} />} />
+
         </Routes>
       </div>
       <Footer />
